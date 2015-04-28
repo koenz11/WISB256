5a24623cd0495bf155e74055e08f33d6c53191f6
koenz11 <koenz11@hotmail.com>
2015-04-28 08:02:32 +0000

    added Sieve of Sundaram

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -3,16 +3,29 @@
 import sys
 import time
 import math
+import itertools
     
-def SieveE(tot):
-    numberList = range(2, tot +1) 
+def SieveEratostheses(n):
+    numberList = range(2, n +1) 
     primeList = []
-    maxCalc = math.sqrt(tot)
-    while len(numberList) > 0 and numberList[0]< maxCalc: 
+    #after sqrt(n) every multiplication is on the other side i.e. 10*90 -> 90*10, so there are only primes left
+    maxCalc = math.sqrt(n) 
+    while len(numberList) > 0 and numberList[0]< maxCalc:
+         # the first number is always a prime
         prime = numberList[0]
         primeList.append(prime)
-        numberList= [x for x in numberList if x%prime !=0] #remove all items divisible by prime 
-    return primeList+numberList #all primes below root(n) + above root(n)
+        #remove all items divisible by prime 
+        numberList= [i for i in numberList if (i%prime !=0)] 
+    #all primes below root(n) + above root(n)
+    return primeList+numberList 
+    
+def SieveSundaram(n):
+    highestNr = int((n-1)*0.5) # because you multiply everything by 2 and add 1, you need only go to (n-1)/2 to get all primes below n
+    numberList = range(1, highestNr) 
+    #calculate all numbers that should be removed
+    removeList = [(i + j + 2*i*j) for i, j in itertools.product(range(1, highestNr), range(1, highestNr)) if (i + j + 2*i*j) < highestNr]
+    numberList= [2*y+1 for y in numberList if (y not in removeList)]
+    return [2] + numberList
 
 #//////////arguments
 arguments = sys.argv
@@ -22,14 +35,26 @@ fileName = sys.argv[2]
 #/////////
 
 T1 = time.perf_counter()
-
-
-primeList = SieveE(highestNr)
-
+primeList = SieveEratostheses(highestNr)
 T2 = time.perf_counter()
 
+'''
+if you comment out the Sieve for Sunderum stuff, you'll see that the algorithm 
+found by Sundaram is slower then the one found by Eratostheses.
+'''
+'''
+T3 = time.perf_counter()
+primeList2 = SieveSundaram(highestNr)
+T4 = time.perf_counter()
+'''
 file = open(fileName, 'w')
 for prime in primeList:
     file.write(str(prime) + '\n')
 
 print('Found', len(primeList), 'Prime numbers smaller than', highestNr, 'in', T2 - T1, 'sec.')
+
+
+'''
+print('SieveEratostheses Found', len(primeList), 'Prime numbers smaller than', highestNr, 'in', T2 - T1, 'sec.')
+print('SieveSundaram Found', len(primeList2), 'Prime numbers smaller than', highestNr, 'in', T4 - T3, 'sec.')
+''' 
--- Opdracht2/countPrimes.py
+++ Opdracht2/countPrimes.py
@@ -11,28 +11,27 @@ file = open(fileName, 'r')
 lastPrime = 0
 primeCount = 0
 twinCount = 0
+#count all the primes
 for line in file:
     prime = int(line)
     primeCount+= 1
+    #check if this prime is twin with the last prime
     if lastPrime + 2 == prime:
         twinCount += 1
     lastPrime = prime
-##########################################################################
-total = 1000000
-#########################################################################
-totalLog = math.log(total)
+    
+#calculate the elements needed for the formula
+lastLog = math.log(lastPrime)
 constant = 0.6601618
-primeFormulaPart = total / totalLog
-primeFormula = primeFormulaPart/primeCount  
-twinFormulaPart = 2* constant* total / (totalLog*totalLog)
-twinFormula = twinFormulaPart/twinCount
+primeFormula = lastPrime / lastLog
+twinFormula = 2* constant* lastPrime / (lastLog*lastLog)
 
-print 'Largest Prime =', lastPrime 
-print '--------------------------------'
-print 'pi(N)         =', primeCount
-print 'N/Log(N)      =', primeFormulaPart
-print 'ratio         =', primeFormula
-print '--------------------------------'
-print 'pi_2(N)       =', twinCount
-print '2CN/Log(N)^2  =', twinFormulaPart
-print 'ratio         =', twinFormula
+print('Largest Prime =', lastPrime)
+print('--------------------------------')
+print('pi(N)         =', primeCount)
+print('N/Log(N)      =', primeFormula)
+print('ratio         =', primeCount/primeFormula)
+print('--------------------------------')
+print('pi_2(N)       =', twinCount)
+print('2CN/Log(N)^2  =', twinFormula)
+print('ratio         =', twinCount/twinFormula)
