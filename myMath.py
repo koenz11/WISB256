0c181ce341c71023992e1e2aadb5af204d2ae7f6
koenz11 <koenz11@hotmail.com>
2015-04-28 08:08:57 +0000

    added speedups found during exercises

--- myMath.py
+++ myMath.py
@@ -39,6 +39,12 @@ def numDivisors(nr):
     
 
 def primefactors(nr):
+    global maxPrime
+    global savedPrimes
+    if maxPrime < nr:
+        savedPrimes = primeNumbers(nr)
+        maxPrime = max(savedPrimes)
+    
     Dict = {}
     remaining = nr
     prime = 2
@@ -55,17 +61,35 @@ def primefactors(nr):
     return Dict
 
 
-
 def primeNumbers(tot):
-    SieveE(tot)
+    return SieveEratostheses(tot)
     
     
-def SieveE(tot):
-    numberList = range(2, tot +1) 
+def SieveEratostheses(n):
+    numberList = range(2, n +1) 
     primeList = []
-    maxCalc = math.sqrt(tot)
-    while len(numberList) > 0 and numberList[0]< maxCalc: 
+    #after sqrt(n) every multiplication is on the other side i.e. 10*90 -> 90*10, so there are only primes left
+    maxCalc = math.sqrt(n) 
+    while len(numberList) > 0 and numberList[0]< maxCalc:
+         # the first number is always a prime
         prime = numberList[0]
         primeList.append(prime)
-        numberList= [x for x in numberList if x%prime !=0] #remove all items divisible by prime 
-    return primeList+numberList #all primes below root(n) + above root(n)
+        #remove all items divisible by prime 
+        numberList= [i for i in numberList if (i%prime !=0)] 
+    #all primes below root(n) + above root(n)
+    return primeList+numberList 
+    
+def SieveSundaram(n):
+    highestNr = int((n-1)*0.5) # because you multiply everything by 2 and add 1, you need only go to (n-1)/2 to get all primes below n
+    numberList = range(1, highestNr) 
+    #calculate all numbers that should be removed
+    removeList = [(i + j + 2*i*j) for i, j in itertools.product(range(1, highestNr), range(1, highestNr)) if (i + j + 2*i*j) < highestNr]
+    numberList= [2*y+1 for y in numberList if (y not in removeList)]
+    return [2] + numberList
+    
+    
+    
+    
+    
+    
+    
